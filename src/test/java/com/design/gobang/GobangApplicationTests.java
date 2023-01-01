28862672ce13986036057f69a350c1f24c2d8105
sunyibud <sy2252757853@gmail.com>
2023-01-01 11:12:04 +0800

    Initial commit

---
+++ src/test/java/com/design/gobang/GobangApplicationTests.java
@@ -0,0 +1,15 @@
+package com.design.gobang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GobangApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
